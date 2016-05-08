be55852e7c416192ecb45a1d771de8b9a3691552
Sharon <sk3513@columbia.edu>
2016-05-07 22:13:59 -0400

    changed template to avoid unset node variable

--- sites/all/themes/elat/templates/page.tpl.php
+++ sites/all/themes/elat/templates/page.tpl.php
@@ -150,9 +150,11 @@
       <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
-      <?php if (!empty($title) && !($is_front) && ($node->type != 'case_study')): ?>
+      <?php if(!empty($node) && $node->type == 'case_study'){ $case_study=TRUE;} else {$case_study = FALSE;}
+      		if(!empty($title) && !($is_front) && $case_study == FALSE): ?>
         <h1 class="page-header"><?php print $title; ?></h1>
       <?php endif; ?>
+      <?php dpm($case_study); ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php if (!empty($tabs)): ?>
