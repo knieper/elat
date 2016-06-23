210390efd34362ee3532e3ebfd07b177aa4d4fe0
Sharon <sk3513@columbia.edu>
2016-06-23 13:44:51 -0400

    edited temporary GA code to track multiple domains https://support.google.com/analytics/answer/1034342?hl=en

--- sites/all/themes/elat/templates/html.tpl.php
+++ sites/all/themes/elat/templates/html.tpl.php
@@ -74,7 +74,9 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-8866736-8', 'auto');
+  ga('create', 'UA-8866736-8', 'auto', {'allowLinker': true});
+  ga('require', 'linker');
+  ga('linker:autoLink', ['crlt.umich.edu'] );
   ga('send', 'pageview');
   </script>
 </head>
